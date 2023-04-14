c3a1eabd049ac1d2ba0ea4336c84860447cc56a1
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-04-14 15:30:04 +0700

    inject service dbcontext

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,9 @@
+using API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<Tugas6Context>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
