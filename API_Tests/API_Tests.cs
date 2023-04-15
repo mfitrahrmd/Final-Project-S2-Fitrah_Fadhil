16eb4dbb594a17bb1937c9dde3638b854160858d
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-04-15 16:39:14 +0700

    define service provider & DI

---
+++ API_Tests/API_Tests.cs
@@ -0,0 +1,21 @@
+using API.Data;
+using API.Repositories.Contracts;
+using API.Repositories.Implementations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API_Tests;
+
+public abstract class API_Tests
+{
+    protected readonly IServiceProvider _service;
+
+    protected API_Tests()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddDbContext<InMemoryTugas6Context>();
+        serviceCollection.AddTransient<CoreRepository<int, Entity, InMemoryTugas6Context>, CoreRepositoryImpl<int, Entity, InMemoryTugas6Context>>();
+        serviceCollection.AddTransient<IUniversityRepository, UniversityRepository<InMemoryTugas6Context>>();
+
+        _service = serviceCollection.BuildServiceProvider();
+    }
+}
