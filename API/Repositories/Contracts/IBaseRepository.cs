003e3c5c09aa27b09912f729f9f0b1611d97626f
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-04-15 16:29:16 +0700

    update TEntity should extend entity interface

--- API/Repositories/Contracts/IBaseRepository.cs
+++ API/Repositories/Contracts/IBaseRepository.cs
@@ -1,10 +1,11 @@
 namespace API.Repositories.Contracts;
 
-public interface IBaseRepository<TPk, TEntity> where TEntity : class
+public interface IBaseRepository<TPk, TEntity> where TEntity : IEntity<TPk>
 {
     Task<TEntity?> InsertOne(TEntity entity);
     Task<TEntity?> FindOneByPk(TPk pk);
     Task<IEnumerable<TEntity>> FindAll();
+    Task<IEnumerable<TEntity>> FindAll(int limit, int offset);
     Task<TEntity?> UpdateOneByPk(TPk pk, TEntity entity);
     Task<TEntity?> DeleteOneByPk(TPk pk);
 }
