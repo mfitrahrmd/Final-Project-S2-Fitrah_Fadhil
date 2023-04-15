e86cfac367ff1e280bf44439d9901cdc6cef00fd
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-04-16 00:21:56 +0700

    inject account repository to service builder DI

--- API/Program.cs
+++ API/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository<Tugas6Context
 builder.Services.AddScoped<IRoleRepository, RoleRepository<Tugas6Context>>();
 builder.Services.AddScoped<IEducationRepository, EducationRepository<Tugas6Context>>();
 builder.Services.AddScoped<IProfilingRepository, ProfilingRepository<Tugas6Context>>();
+builder.Services.AddScoped<IAccountRepository, AccountRepository<Tugas6Context>>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
