1a4079bbd66af2c4469a5464d0b4f5913efc813b
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-04-15 23:15:04 +0700

    reformat code

--- API/Repositories/Implementations/UniversityRepository.cs
+++ API/Repositories/Implementations/UniversityRepository.cs
@@ -1,11 +1,11 @@
-using API.Data;
 using API.Models;
 using API.Repositories.Contracts;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Repositories.Implementations;
 
-public class UniversityRepository<TContext> : CoreRepository<int, TbMUniversity, TContext>, IUniversityRepository where TContext : DbContext
+public class UniversityRepository<TContext> : CoreRepository<int, TbMUniversity, TContext>, IUniversityRepository
+    where TContext : DbContext
 {
     public UniversityRepository(TContext context) : base(context)
     {
