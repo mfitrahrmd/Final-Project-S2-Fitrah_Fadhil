975fd546406901e266c814b7cacc49575cb4d872
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-04-18 10:44:19 +0700

    implement account controller

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+using API.DTOs;
+using API.Exceptions;
+using API.Models;
+using API.Repositories.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AccountController : Controller
+{
+    private readonly IAccountRepository _accountRepository;
+
+    public AccountController(IAccountRepository accountRepository)
+    {
+        _accountRepository = accountRepository;
+    }
+
+    [Route("Register")]
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterInput data)
+    {
+        TbMEmployee? registered = null;
+        try
+        {
+            registered = await _accountRepository.RegisterAsync(data.ToEmployeeEntity());
+
+            if (registered is null)
+                return BadRequest();
+        }
+        catch (RepositoryException e)
+        {
+            Console.WriteLine(e.Message);
+            return BadRequest();
+        }
+        return Created("", new RegisterOutput
+        {
+            Email = registered.Email
+        });
+    }
+
+    [Route("Login")]
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginInput data)
+    {
+        var loggedIn = await _accountRepository.LoginAsync(data.ToEmployeeEntity());
+
+        if (loggedIn is null)
+            return BadRequest();
+
+        return Ok(new LoginOutput
+        {
+            Email = loggedIn.Email
+        });
+    }
+}
