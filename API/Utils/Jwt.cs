e9590a9c2d5f7ef7b44fb31cde25068d0332d457
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-04-18 13:19:23 +0700

    implement jwt utility

---
+++ API/Utils/Jwt.cs
@@ -0,0 +1,34 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Utils;
+
+public record Payload(string FullName, string Email, string Role);
+
+public class Jwt
+{
+    private readonly IConfiguration _config;
+
+    public Jwt(IConfiguration config)
+    {
+        _config = config;
+    }
+    
+    public string GenerateToken(Payload payload)
+    {
+        var key = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["jwt:Key"])),
+            SecurityAlgorithms.HmacSha256);
+        var claims = new[]
+        {
+            new Claim(ClaimTypes.Name, payload.FullName),
+            new Claim(ClaimTypes.Email, payload.Email),
+            new Claim(ClaimTypes.Role, payload.Role)
+        };
+        var token = new JwtSecurityToken(_config["jwt:Issuer"], _config["jwt:Audience"], claims,
+            DateTime.Now.AddMinutes(_config.GetValue<double>("jwt:ExpiresInMinute")), signingCredentials:key);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
