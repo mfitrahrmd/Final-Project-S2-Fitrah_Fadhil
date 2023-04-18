9db3de6113693b0389a5fcb28cca1270585f491e
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-04-18 12:11:50 +0700

    use existing university if possible&give 'User' role for default registration

--- API/Repositories/Implementations/AccountRepository.cs
+++ API/Repositories/Implementations/AccountRepository.cs
@@ -9,10 +9,14 @@ public class AccountRepository<TContext> : CoreRepository<string, TbMAccount, TC
     where TContext : DbContext
 {
     private IEmployeeRepository _employeeRepository;
-    
-    public AccountRepository(TContext context, IEmployeeRepository employeeRepository) : base(context)
+    private IUniversityRepository _universityRepository;
+    private IRoleRepository _roleRepository;
+
+    public AccountRepository(TContext context, IEmployeeRepository employeeRepository,
+        IUniversityRepository universityRepository, IRoleRepository roleRepository) : base(context)
     {
-        _employeeRepository = employeeRepository;
+        (_employeeRepository, _universityRepository, _roleRepository) =
+            (employeeRepository, universityRepository, roleRepository);
     }
 
     public async Task<TbMEmployee?> RegisterAsync(TbMEmployee employee)
@@ -20,18 +24,37 @@ public class AccountRepository<TContext> : CoreRepository<string, TbMAccount, TC
         TbMEmployee? registeredAccount;
         try
         {
+            // check for duplicate nik
             var foundWithNik = await _employeeRepository.FindOneByPk(employee.Nik);
             if (foundWithNik is not null)
                 throw new RepositoryException("Nik already exist");
 
+            // check for duplicate email
             var foundWithEmail = await _employeeRepository.FindOneByEmailAsync(employee.Email);
             if (foundWithEmail is not null)
                 throw new RepositoryException("email already exist");
-            
+
+            // check for duplicate phone number
             var foundWithPhoneNumber = await _employeeRepository.FindOneByPhoneNumberAsync(employee.PhoneNumber);
             if (foundWithEmail is not null)
                 throw new RepositoryException("phone number already exist");
-            
+
+            // check if university already exist, if does use that existing university
+            var foundUniversity =
+                await _universityRepository.FindOneByNameAsync(employee.TbTrProfiling.Education.University.Name);
+            if (foundUniversity is not null)
+            {
+                employee.TbTrProfiling.Education.University = null;
+                employee.TbTrProfiling.Education.UniversityId = foundUniversity.Id;
+            }
+
+            // Default role 'User' for registration
+            var userRole = await _roleRepository.FindOneOrInsertByName("User");
+            employee.TbMAccount.TbTrAccountRoles.Add(new TbTrAccountRole
+            {
+                RoleId = userRole.Id
+            });
+
             registeredAccount = await _employeeRepository.InsertOne(employee);
         }
         catch (Exception e)
