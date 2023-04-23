9efda6dabf29d3fd817b466148240a32fb0a0881
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-04-23 14:28:02 +0700

    add authorization

--- API/Controllers/UniversitiesController.cs
+++ API/Controllers/UniversitiesController.cs
@@ -3,10 +3,12 @@ using API.DTOs.request;
 using API.Models;
 using API.Repositories.Contracts;
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
+[Authorize]
 [Route("api/[controller]")]
 [ApiController]
 public class UniversitiesController : CoreController<IUniversityRepository, int, TbMUniversity, UniversityDTO, InsertUniversityRequest, UpdateUniversityRequest>
