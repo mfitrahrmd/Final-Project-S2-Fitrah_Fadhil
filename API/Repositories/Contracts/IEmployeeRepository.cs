81c92ca3b688badd6da7e90b61e1056fe5a175dd
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-04-23 15:59:50 +0700

    add FindManyByAboveAvgGpaAndHiringYear method

--- API/Repositories/Contracts/IEmployeeRepository.cs
+++ API/Repositories/Contracts/IEmployeeRepository.cs
@@ -7,4 +7,5 @@ public interface IEmployeeRepository : IBaseRepository<string, TbMEmployee>
     Task<TbMEmployee?> FindOneByEmailAsync(string email);
     Task<TbMEmployee?> FindOneByPhoneNumberAsync(string phoneNumber);
     Task<IEnumerable<TbMEmployee>> FindManyIncludeEducationAndUniversityAsync();
+    Task<IEnumerable<TbMEmployee>> FindManyByAboveAvgGpaAndHiringYear(int year);
 }
--- API/Repositories/Implementations/EmployeeRepository.cs
+++ API/Repositories/Implementations/EmployeeRepository.cs
@@ -30,4 +30,18 @@ public class EmployeeRepository<TContext> : CoreRepository<string, TbMEmployee, 
             .ThenInclude(e => e.University)
             .AsEnumerable();
     }
+    
+    public async Task<IEnumerable<TbMEmployee>> FindManyByAboveAvgGpaAndHiringYear(int year)
+    {
+        var avgGpa = _dbSet.Select(e => e.TbTrProfiling.Education.Gpa).AsEnumerable().Average();
+        
+        var employees = _dbSet
+            .Include(e => e.TbTrProfiling.Education)
+            .Include(e => e.TbTrProfiling.Education.University)
+            .Where(e => e.HiringDate.Year.Equals(year))
+            .Where(e => e.TbTrProfiling.Education.Gpa > avgGpa)
+            .AsEnumerable();
+
+        return employees;
+    }
 }
