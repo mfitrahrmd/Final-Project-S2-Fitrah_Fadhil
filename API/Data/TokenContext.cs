47eba02329c78c16ee7943338981a49ee84f7e83
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-09 02:23:28 +0700

    define refresh token db context

---
+++ API/Data/TokenContext.cs
@@ -0,0 +1,26 @@
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class TokenContext : DbContext
+{
+    private readonly IConfiguration _config; 
+    
+    public TokenContext(IConfiguration config)
+    {
+        _config = config;
+    }
+    
+    public TokenContext(DbContextOptions<TokenContext> options, IConfiguration config) : base(options)
+    {
+        _config = config;
+    }
+
+    public virtual DbSet<UserToken> UserTokens { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data Source={_config["jwt:RefreshTokenDBPath"]}");
+    }
+}
